157d87c7cd6cb9b647e10ecc5eed14519911d6fc
JianShuiHuang <47372304+JianShuiHuang@users.noreply.github.com>
2020-08-31 13:15:50 +0800

    Update Main_eye.py

--- Main_eye.py
+++ Main_eye.py
@@ -30,7 +30,7 @@ Momentum = 0.9
 Weight_decay = 5e-4
 in_feature = 3 * 64 * 64
 out_feature = 1000
-"""
+
 transformations = transforms.Compose([transforms.ToTensor()])
 train_np = DataLoader('data/', 'train', transformations)
 test_np = DataLoader('data/', 'test', transformations)
@@ -41,14 +41,15 @@ input_size = train_np[0][0].shape[0] * train_np[0][0].shape[1] * train_np[0][0].
 output_size = 5
 hidden_size = 1000    
     
-    
+print(input_size)
+ 
 train_data = np.zeros((num_images_train, input_size))
 train_label = np.zeros(num_images_train)
 for i in range(num_images_train):
     if i % 100 == 0:
         print(i)
-        train_data[i] = train_np[i][0].flatten()
-        train_label[i] = 1 if train_np[i][1] >= 1 else 0
+    train_data[i] = train_np[i][0].flatten()
+    train_label[i] = 1 if train_np[i][1] >= 1 else 0
 
 np.save("Retinopathy_train_data.npy", train_data)
 np.save("Retinopathy_train_label.npy", train_label)
@@ -59,19 +60,19 @@ test_label = np.zeros(num_images_test)
 for i in range(num_images_test):
     if i % 100 == 0:
         print(i)
-        test_data[i] = test_np[i][0].flatten()
-        test_label[i] = 1 if test_np[i][1] >= 1 else 0
+    test_data[i] = test_np[i][0].flatten()
+    test_label[i] = 1 if test_np[i][1] >= 1 else 0
 
 np.save("Retinopathy_test_data.npy", test_data)
 np.save("Retinopathy_test_label.npy", test_label)
-"""     
+   
     
-
+"""
 train_data = np.load("Retinopathy_train_data.npy")
 train_label = np.load("Retinopathy_train_label.npy")
 test_data = np.load("Retinopathy_test_data.npy")
 test_label = np.load("Retinopathy_test_label.npy")
-
+"""
 print("data load complet...")
  
 train_accuracy = []
